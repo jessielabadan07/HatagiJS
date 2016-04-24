d87acf16daa50839821bb1923b77dee2c87186e7
JessieLabadan07 <jessielabadan07@gmail.com>
2016-04-24 11:43:48 +0800

    Added constructor

--- hatagi.js
+++ hatagi.js
@@ -5,14 +5,22 @@
  *
 **/
 
-//var HatagiNamespace = HatagiNamespace || {};
-
-(function() {
+(function(HatagiNamespace) {
 
 	var Hatagi = (function() {
 
+		function Hatagi() {
+			this.list = [];
+		};
 
+		Hatagi.prototype.init = function() {
+
+		};
+
+		return Hatagi;
 
 	})();
+
+	HatagiNamespace.Hatagi = Hatagi;
 
 })(window.HatagiNamespace || (window.HatagiNamespace = {}));
