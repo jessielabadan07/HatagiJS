fb8f182a59c0a9d0ff728e2c4b20e2dd80bfdf38
JessieLabadan07 <jessielabadan07@gmail.com>
2016-04-24 12:20:46 +0800

    Added constructor

--- hatagi.js
+++ hatagi.js
@@ -5,20 +5,49 @@
  *
 **/
 
-(function(HatagiNamespace) {
+(function(HApp) {
 
-  var Hatagi = (function() {
-  	
-    function Hatagi() {
-	  this.list = [];
-	};
+	var Hatagi = (function() {
 
-	Hatagi.prototype.init = function() { };
+		var lists = [];
 
-	return Hatagi;
+		function Hatagi(args) {
+	  		var args = args || arguments;	  		
+			for(var i = 0; i < args.length; i++) {
+				lists.push(args[i]);
+			}		
 
-  })();
+			console.log(lists);
+		};
 
-  HatagiNamespace.Hatagi = Hatagi;
+		Hatagi.prototype.init = function(args) { 			
+			var args = args || arguments;
+			for(var i = 0; i < args.length; i++) {
+				this.lists.push(args[i]);
+			}			
+		};
 
-})(window.HatagiNamespace || (window.HatagiNamespace = {}));
+		var instance;
+
+		var _static = {
+
+			name: 'Hatagi',
+
+			getInstance: function (args) {
+				if(instance === undefined) {
+					instance = new Hatagi(args);
+				}
+				return instance;
+			}
+
+		}
+
+		return _static;
+
+  	})();		
+
+  	HApp.init = function() {  		    		
+		return Hatagi.getInstance( ([].slice.call(arguments[0])) );
+  	};
+
+})(window.HApp || (window.HApp = {}));
